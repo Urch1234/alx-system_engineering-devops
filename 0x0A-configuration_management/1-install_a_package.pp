1cee22dd49f16b828d4962318d5ee15824e31a75
Ihezie Ernest Uchenna <iernest61@gmail.com>
2024-05-08 00:10:38 -0700

    1-install_a_package.pp edited

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # Using Puppet, install flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'Install flask':
+  ensure   	=> '2.1.0',
+	name			=> 'flask',
+  provider 	=> 'pip3',
 }
