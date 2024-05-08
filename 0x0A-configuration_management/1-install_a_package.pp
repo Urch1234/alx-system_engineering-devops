93a05f650abe442823ed320799b69267ddecda8d
Ihezie Ernest Uchenna <iernest61@gmail.com>
2024-05-08 01:19:31 -0700

    1-install_a_package.pp edited

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
-#!/usr/bin/pup
-# Using Puppet, install flask from pip3
+# Define a resource class for managing pip packages
+class { 'pip': }
+
+# Define a package resource for Flask
 package { 'Flask':
-  ensure   => '2.1.0',
+  ensure => '2.1.0',
   provider => 'pip3',
 }
