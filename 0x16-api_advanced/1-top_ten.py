96e96c66c242fffb1cd3bd9adcff531ee4560e69
Ihezie Ernest Uchenna <iernest61@gmail.com>
2024-06-24 09:33:08 -0700

    1-top_ten.py edited

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,30 +4,44 @@
 prints the titles of the first 10 hot posts listed for a given subreddit
 """
 
-from requests import get
+import requests
 
 
 def top_ten(subreddit):
     """
-    function that queries the Reddit API and prints the titles of the first
-    10 hot posts listed for a given subreddit
+    Function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        None
     """
 
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
+        return
 
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-
-    response = get(url, headers=user_agent, params=params)
-    results = response.json()
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
 
     try:
-        my_data = results.get('data').get('children')
+        response = requests.get(url, headers=user_agent, params=params, allow_redirects=False)
+        if response.status_code != 200:
+            print("None")
+            return
 
-        for i in my_data:
-            print(i.get('data').get('title'))
+        results = response.json()
+        posts = results.get('data', {}).get('children', [])
 
-    except Exception:
+        if not posts:
+            print("None")
+            return
+
+        for post in posts:
+            print(post.get('data', {}).get('title', "None"))
+
+    except (requests.RequestException, ValueError, KeyError):
         print("None")
